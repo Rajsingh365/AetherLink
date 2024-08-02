ac5c21d36768592b81d7ff17c4ff1ed61ba4995d
Rajsingh365 <sraj08188@gmail.com>
2024-08-02 07:40:01 +0530

    signup endpoint complete

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+dotenv.config();
+
+app.use(express.json())
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
