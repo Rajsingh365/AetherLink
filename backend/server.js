b619ab6909f79c4de38bf96c106f7926cd9dad57
Rajsingh365 <sraj08188@gmail.com>
2024-08-02 23:05:58 +0530

    user endpoint complete

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from './routes/messages.route.js'
+import userRoutes from './routes/user.routes.js'
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -15,7 +17,7 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-// app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
