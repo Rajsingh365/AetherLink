446ba8f1c23a1c3f1bd4f99885dbe12fa2ffeb2b
Rajsingh365 <sraj08188@gmail.com>
2024-08-02 22:37:42 +0530

    send message endpoint complete

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
+import messageRoutes from './routes/messages.route.js'
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 dotenv.config();
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+// app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
