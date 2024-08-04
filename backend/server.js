f406db66e251b87a559020e2251db572a2040252
Rajsingh365 <sraj08188@gmail.com>
2024-08-04 11:29:41 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,7 @@ import {server, app} from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();   //gives absolute path of the current directory
 
 dotenv.config();
 
@@ -21,9 +23,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 
 server.listen(PORT, () => {
